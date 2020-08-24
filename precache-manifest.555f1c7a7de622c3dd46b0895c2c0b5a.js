1570506efd1a5cc6f7401a1bc9f0080e15765270
Chris Gonzales <chrisgshred@gmail.com>
2020-08-24 08:22:04 -0700

    Updates

---
+++ precache-manifest.555f1c7a7de622c3dd46b0895c2c0b5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e440111daea8811efb2bb819c74ee8d4",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "dcbae69b23c04f26fd99",
+    "url": "/react-portfolio/static/css/2.f68e0724.chunk.css"
+  },
+  {
+    "revision": "a9db4b1e9aace93a9395",
+    "url": "/react-portfolio/static/css/main.bdd94370.chunk.css"
+  },
+  {
+    "revision": "dcbae69b23c04f26fd99",
+    "url": "/react-portfolio/static/js/2.c05af2a6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.c05af2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9db4b1e9aace93a9395",
+    "url": "/react-portfolio/static/js/main.f7a9b7d8.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react-portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "2096cb3bde5621966715eae4f8e5a05f",
+    "url": "/react-portfolio/static/media/slide-2.2096cb3b.jpg"
+  }
+]);
